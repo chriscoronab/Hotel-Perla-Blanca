ae1a8abc9376eaa3423e632313e2434254e11a9b
Christian Corona <chriscoronab@gmail.com>
2022-12-28 00:00:47 -0300

    correcciones

--- js/script.js
+++ js/script.js
@@ -4,99 +4,92 @@ function bienvenida(){
 
 bienvenida();
 
-let nombreCompleto=prompt("Por favor, ingresa tu nombre completo para acceder a más información sobre nuestros servicios.");
-
-alert("Bienvenido/a, "+nombreCompleto);
-
-let cantidadPersonas=parseInt(prompt(nombreCompleto+", ingresa la cantidad de personas a hospedarse en nuestro Hotel."));
-switch(cantidadPersonas){
-    case 1:
-        alert("Ingresaste 1 persona. \nTenemos habitaciones disponibles.");
-        break;
-    case 2:
-        alert("Ingresaste 2 personas. \nTenemos habitaciones dobles disponibles.");
-        break;
-    case 3:
-        alert("Ingresaste 3 personas. \nTenemos habitaciones triples disponibles.");
-        break;
-    case 4:
-        alert("Ingresaste 4 personas. \nTenemos habitaciones cuádruples disponibles.");
-        break;
-    case 0:
-        alert("Error. Ingresa aunque sea 1 persona.");
-    default:
-        alert("No tenemos habitaciones para más de 4 personas. Te podemos ofrecer varias habitaciones para hospedar a tu grupo.");
-        break;
+class Reserva{
+    constructor(titular, numeroPersonas, noches, fechaIngreso, fechaSalida, numeroReserva){
+        this.titular=titular;
+        this.numeroPersonas=parseInt(numeroPersonas);
+        this.noches=parseInt(noches);
+        this.fechaIngreso=fechaIngreso;
+        this.fechaSalida=fechaSalida;
+        this.numeroReserva=numeroReserva;
+    }
+    asignarNumeroReserva(reservas){
+        this.numeroReserva=reservas.length;
+    }
 }
+
+const reservas=[];
+
+let ingreso=prompt("Por favor, ingresa los siguientes datos para reservar tu estadía en el Hotel: nombre y apellido del titular, cantidad de personas, cantidad de noches, fecha de ingreso y fecha de salida, separados por una barra diagonal (/).");
+
+let datos=ingreso.split("/");
+
+const reserva=new Reserva(datos[0], datos[1], datos[2], datos[3], datos[4]);
+reservas.push(reserva);
+reserva.asignarNumeroReserva(reservas);
 
 const habitaciones=[{
     id: 1,
     nombre: "Habitación Doble",
-    precio: "USD $35",
+    precio: 35,
 }, {
     id: 2,
     nombre: "Habitación Triple",
-    precio: "USD $40",
+    precio: 40,
 }, {
     id: 3,
     nombre: "Habitación Cuádruple",
-    precio: "USD $50",
+    precio: 50,
 }, {
     id: 4,
     nombre: "Habitación Suite",
-    precio: "USD $85",
+    precio: 85,
 },
 ]
 
-const servicios=["Aire acondicionado", "Baño privado", "TV LED con cable", "Cama sommier", "Frigobar", "Pava eléctrica", "Salón desayunador", "Sala de juegos", "Wi-Fi", "Estacionamiento cubierto"];
+let criterio=prompt("Elige el criterio deseado para ver las habitaciones disponibles: \n1- De menor a mayor precio. \n2- De mayor a menor precio.");
 
-alert("Todas las habitaciones cuentan con: "+servicios.join("\n"));
-
-let nochesHospedaje=parseInt(prompt(nombreCompleto+", ingresa la cantidad de noches de hospedaje en el Hotel Perla Blanca:"));
-
-for(let i=1; i<=3;i++){
-    if(nochesHospedaje>0){
-        alert("Tu estadía es de "+nochesHospedaje+" noches.");
-        break;
-    }else{
-        alert("Error. Ingresa aunque sea 1 noche.");
-        nochesHospedaje=parseInt(prompt(nombreCompleto+", ingresa la cantidad de noches de hospedaje en el Hotel Perla Blanca:"));
+function ordenar(criterio, habitaciones){
+    let arrayOrdenado=habitaciones.slice(0);
+    switch(criterio){
+        case "1":
+            return arrayOrdenado.sort((a, b)=>a.precio-b.precio);
+        case "2":
+            return arrayOrdenado.sort((a, b)=>b.precio-a.precio);
+        default:
+            alert("Error. No es un criterio válido.");
+            criterio=prompt("Elige el criterio deseado para ver las habitaciones disponibles: \n1- De menor a mayor precio. \n2- De mayor a menor precio.");
     }
 }
 
-const IVA=1.21;
-let precioNoche=35;
-let precioTotal=(precioNoche*nochesHospedaje)*IVA;
-
-function calcularPrecioTotal(precio){
-    alert("El precio por noche es de USD $"+precioNoche+", por lo que el precio total en nuestras instalaciones incluyendo el IVA es de USD $"+precioTotal);
+function crearResultado(habitaciones){
+    let info=""
+    habitaciones.forEach(elemento=>{
+        info+="Tipo: "+elemento.nombre+". Precio: USD $"+elemento.precio+".\n\n"
+    });
+    return info;
 }
 
-calcularPrecioTotal();
+alert(crearResultado(ordenar(criterio, habitaciones)));
 
-let numeroCuotas;
+const servicios=["Aire acondicionado", "Baño privado", "TV LED con cable", "Cama sommier", "Frigobar", "Pava eléctrica", "Salón desayunador", "Sala de juegos", "Wi-Fi", "Estacionamiento cubierto"];
 
-function calcularCuotas(precioPorCuota){
-    let valorCuotas=precioTotal/numeroCuotas;
-    alert("Ingresaste "+numeroCuotas+" cuotas. \nEl valor de cada cuota es de USD $"+valorCuotas);
-}
-
-let descuentoEfectivo=precioTotal*0.90;
+alert("Todas las habitaciones cuentan con: \n"+servicios.join("\n"));
 
 let metodoDePago=prompt("Elige el método de pago: \n1- Efectivo. \n2- Débito. \n3- Crédito.");
 if((metodoDePago.toLowerCase()=="efectivo")||(metodoDePago=="1")){
-    alert("El método de pago ingresado es efectivo. \nSe aplica un descuento en el valor total realizando el pago en efectivo, por lo que quedaría en USD $"+descuentoEfectivo);
+    alert("El método de pago ingresado es efectivo.");
 }else if((metodoDePago.toLowerCase()=="debito")||(metodoDePago.toLowerCase()=="débito")||(metodoDePago=="2")){
     alert("El método de pago ingresado es tarjeta de débito.");
 }else if((metodoDePago.toLowerCase()=="credito")||(metodoDePago.toLowerCase()=="crédito")||(metodoDePago=="3")){
-    numeroCuotas=parseInt(prompt("El método de pago ingresado es tarjeta de crédito. \nIngresa el número de cuotas: 1, 3, 6 o 12"));
+    numeroCuotas=parseInt(prompt("El método de pago ingresado es tarjeta de crédito. \nIngresa el número de cuotas: 3, 6 o 12"));
     for(let i=1; i<=3;i++){
-        if((numeroCuotas=="1")||(numeroCuotas=="3")||(numeroCuotas=="6")||(numeroCuotas=="12")){
-            calcularCuotas();
+        if((numeroCuotas=="3")||(numeroCuotas=="6")||(numeroCuotas=="12")){
+            alert("Elegiste: "+numeroCuotas+" cuotas.");
             break;
         }else{
-            alert("Error. El número de cuotas permitido son: 1, 3, 6 o 12.");
-            numeroCuotas=parseInt(prompt("Vuelve a ingresar el número de cuotas: 1, 3, 6 o 12."));
+            alert("Error. El número de cuotas permitido son: 3, 6 o 12.");
+            numeroCuotas=parseInt(prompt("Vuelve a ingresar el número de cuotas: 3, 6 o 12."));
         }
     }
 }else{
@@ -108,12 +101,22 @@ let nombre=prompt("Ingresa el nombre completo de cada una de las personas a hosp
 
 numeroPersonas=0;
 
-while(nombre!="X"&&nombre!="x"){
+while(nombre.toUpperCase()!="X"){
     numeroPersonas=numeroPersonas+1;
     nombre=prompt("Ingresa el nombre completo de cada una de las personas a hospedarse en el Hotel Perla Blanca para el check-in. \nPresiona X para finalizar cuando hayas ingresado todos los nombres.");
 }
 
 alert("¡Perfecto! Estas "+numeroPersonas+" personas deberán presentarse en recepción al momento de hacer el check-in con sus respectivos DNI para validar los datos. Muchas gracias por elegirnos.");
+
+function darDatosReserva(reservas){
+    let datosReserva=""
+    reservas.forEach(elemento=>{
+        datosReserva+="Datos de Reserva: \nNombre del titular: "+elemento.titular+"\nCantidad de personas: "+elemento.numeroPersonas+"\nCantidad de noches: "+elemento.noches+"\nFecha de ingreso: "+elemento.fechaIngreso+"\nFecha de salida: "+elemento.fechaSalida
+    });
+    return datosReserva;
+}
+
+alert(darDatosReserva(reservas));
 
 let numeroReserva=Math.random()*5000;
 
