c6661ec54eb39edfe91e49b2aa375cb4ea2ebded
Christian Corona <chriscoronab@gmail.com>
2023-01-15 00:17:05 -0300

    actualizacion de storage

--- js/script.js
+++ js/script.js
@@ -9,7 +9,8 @@ let datosReserva=document.querySelector("#datosReserva");
 let numeroReserva=Math.round(Math.random()*5000);
 let boton=document.querySelector("#boton");
 let alertFecha=document.querySelector("#alertFecha");
-let divBoton=document.querySelector("#divBoton");
+let reservaHecha=document.querySelector("#reservaHecha");
+let inputsFormulario=document.querySelector("#inputsFormulario");
 
 function calcularNoches(){
     let fechaIngreso=new Date(document.querySelector("#fechaIngreso").value);
@@ -29,7 +30,6 @@ formulario.addEventListener("submit", function (e){
     e.preventDefault();
     alertFecha.innerHTML=`<div class="alert alert-danger d-none" role="alert"><p>
     La fecha de salida debe ser mayor a la fecha de ingreso</p></div>`;
-    divBoton.innerHTML=`<button id="boton" type="submit" class="btn btn-primary d-none">Reservar</button>`;
     datosReserva.innerHTML=`
     <div class="alert alert-success" role="alert"><p>¡Gracias por confiar en nosotros! Te enviamos un correo a ${correoElectronico.value} con los datos de tu reserva.
     <br>
@@ -55,27 +55,44 @@ formulario.addEventListener("submit", function (e){
     <br>
     No olvides presentarte en recepción al momento de hacer el check-in con los respectivos DNI para validar los datos.</p>
     </div>`;
+    inputsFormulario.innerHTML=`<form id="inputsFormulario" class="container d-none">`;
 });
 
 const reservas=[];
 
 boton.addEventListener("click", ()=>{
     const reserva={
-        "Nombre del titular": nombreTitular.value,
-        "Correo electrónico": correoElectronico.value,
-        "Teléfono": telefono.value,
-        "Cantidad de personas": parseInt(cantidadPersonas.value),
-        "Tipo de habitación": tipoHabitacion.value,
-        "Fecha de ingreso": fechaIngreso.value,
-        "Fecha de salida": fechaSalida.value,
-        "Cantidad de noches": parseInt(cantidadNoches.value),
-        "Método de pago": metodoPago.value,
-        "Número de reserva": numeroReserva
+        "nombre": nombreTitular.value,
+        "email": correoElectronico.value,
+        "telefono": telefono.value,
+        "personas": parseInt(cantidadPersonas.value),
+        "habitacion": tipoHabitacion.value,
+        "ingreso": fechaIngreso.value,
+        "salida": fechaSalida.value,
+        "noches": parseInt(cantidadNoches.value),
+        "pago": metodoPago.value,
+        "numero reserva": numeroReserva
     }
     reservas.push(reserva);
     localStorage.setItem("reserva", JSON.stringify(reserva));
     sessionStorage.setItem("reserva", JSON.stringify(reserva));
 });
+
+function recuperarDatosReserva(storage){
+    let datosReservaRecuperados=JSON.parse(storage.getItem("reserva"));
+    return datosReservaRecuperados;
+};
+
+function yaReservo(reserva){
+    if(reserva){
+        reservaHecha.innerHTML=`<div class="alert alert-success" role="alert"><p>¡Hola, ${reserva.nombre}! Ya tienes una reserva hecha en el Hotel Perla Blanca desde el ${reserva.ingreso} hasta el ${reserva.salida}</p></div>`;
+        inputsFormulario.innerHTML=`<form id="inputsFormulario" class="container d-none">`;
+    }
+};
+
+window.onload=()=>{
+    yaReservo(recuperarDatosReserva(localStorage));
+};
 
 const habitaciones=[{
     id: 1,
